7f1e861db14a094e639c7aa522ad187b61134c1e
Darkgrouptw <darkgrouptw@gmail.com>
2017-05-10 10:08:34 +0800

    1. 增加 Framework 的格線

--- 0. Shader Framework/Assets/Shaders/Example-Fragment.fs.glsl
+++ 0. Shader Framework/Assets/Shaders/Example-Fragment.fs.glsl
@@ -1,4 +1,5 @@
 #version 410
+in vec3 BarryPos;									// 畫邊界用的
 in vec2 UV;
 
 out vec4 FragColor;
@@ -7,10 +8,20 @@ uniform sampler2D Texture;
 
 uniform uint IsUseTexture;
 
+// 邊界顏色
+vec4 BorderColor = vec4(0, 0, 0, 1);
+float edgeFactor()
+{
+	vec3 d = fwidth(BarryPos);
+	vec3 a3 = smoothstep(vec3(0.0), d * 0.8f, BarryPos);
+	return min(min(a3.x, a3.y), a3.z);
+}
+
 void main()
 {
+	vec4 OutColor = vec4(1, 1, 1, 1);
 	if (IsUseTexture == 1)
 		FragColor = texture2D(Texture, UV);
 	else
-		FragColor = vec4(1, 1, 1, 1);
+		FragColor = mix(BorderColor, OutColor, edgeFactor());;
 }
--- 0. Shader Framework/Assets/Shaders/Example-Vertex.vs.glsl
+++ 0. Shader Framework/Assets/Shaders/Example-Vertex.vs.glsl
@@ -3,15 +3,19 @@ layout(location = 0) in vec3 VertexPos;
 layout(location = 1) in vec2 VertexUV;
 layout(location = 2) in vec3 VertexNormal;
 
+out vec3 BarryPos;
 out vec2 UV;
 
 uniform mat4 ProjectionM;
 uniform mat4 ViewM;
 uniform mat4 ModelM;
 
+const vec3 BarryCoordPos[] = vec3[3](vec3(1, 0, 0), vec3(0, 1, 0), vec3(0, 0, 1));
+
 void main()
 {
     UV = VertexUV;
 
+	BarryPos = BarryCoordPos[gl_VertexID % 3];
 	gl_Position = ProjectionM * ViewM * ModelM * vec4(VertexPos, 1.0);
 }
